0bd162cba97629a304e05ac341e47d8e31af1055
JhonatanTana <jhonatan.tana@alunos.unis.edu.br>
2024-05-11 17:08:18 -0300

    Adicionando DataAnnotations

---
+++ APICatalogo/Models/Categoria.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace APICatalogo.Models;
+
+[Table("Categoria")] //DataAnnotations
+public class Categoria {
+
+    public Categoria() {
+        Produtos = new Collection<Produto>();
+    }
+
+    [Key] // DataAnnotations
+    public int CategoriaId { get; set; }
+
+    [Required]
+    [StringLength(80)] // DataAnnotations
+    public string? Nome { get; set; }
+
+    [Required]
+    [StringLength(300)] // DataAnnotations
+    public string? ImagemUrl { get; set; }
+    public ICollection<Produto>? Produtos { get; set; }  // Define uma chave estrangeira
+}
---
+++ APICatalogo/Models/Produto.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace APICatalogo.Models;
+[Table("Produto")] // DataAnnotations
+public class Produto {
+
+    [Key] // DataAnnotations
+    public int ProdutoId { get; set; }
+    
+    [Required]
+    [StringLength(80)] // DataAnnotations
+    public string? Nome { get; set; }
+
+    [Required]
+    [StringLength(300)] // DataAnnotations
+    public string? Descricao { get; set; }
+    
+    [Required]
+    [Column (TypeName = "decimal(10,2)")] // DataAnnotations
+    public decimal Preco { get; set; }
+    
+    [Required]
+    [StringLength(300)] // DataAnnotations
+    public string? ImagemUrl { get; set; }
+    public float Estoque { get; set; }
+    public DateTime DataCadatro { get; set; }
+    public int CategoriaId { get; set; } // Relaciona a chave estrangeira
+    public Categoria? Categoria { get; set; }  // Relaciona a chave estrangeira
+}
