fb5f313ea1e5f762bc205697376a56442ee09835
JhonatanTana <jhonatan.tana@alunos.unis.edu.br>
2024-05-12 19:04:19 -0300

    Middleware

--- APICatalogo/Program.cs
+++ APICatalogo/Program.cs
@@ -1,4 +1,5 @@
 using APICatalogo.Context;
+using APICatalogo.Extensions;
 using Microsoft.EntityFrameworkCore;
 using System.Text.Json.Serialization;
 
@@ -23,6 +24,7 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment()) {
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.ConfigureExceptionHandler();
 }
 
 app.UseHttpsRedirection();
