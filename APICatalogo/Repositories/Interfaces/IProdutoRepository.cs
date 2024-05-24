3360129967186def75ec417cac4f7293f35c4a10
JhonatanTana <jhonatan.tana@alunos.unis.edu.br>
2024-05-24 12:36:05 -0300

    Paginação

--- APICatalogo/Repositories/Interfaces/IProdutoRepository.cs
+++ APICatalogo/Repositories/Interfaces/IProdutoRepository.cs
@@ -1,7 +1,11 @@
 ﻿using APICatalogo.Models;
+using APICatalogo.Pagination;
 
 namespace APICatalogo.Repositories; 
 public interface IProdutoRepository : IRepository<Produto> {
+
+    //IEnumerable<Produto> GetProdutos(ProdutosParameters produtoParams);
+    PagedList<Produto> GetProdutos(ProdutosParameters produtoParams);
 
     IEnumerable<Produto> GetProdutosPorCategoria(int id);
 }
--- APICatalogo/Repositories/ProdutoRepository.cs
+++ APICatalogo/Repositories/ProdutoRepository.cs
@@ -1,5 +1,6 @@
 ﻿using APICatalogo.Context;
 using APICatalogo.Models;
+using APICatalogo.Pagination;
 
 namespace APICatalogo.Repositories;
 public class ProdutoRepository : Repository<Produto>, IProdutoRepository {
@@ -7,6 +8,21 @@ public class ProdutoRepository : Repository<Produto>, IProdutoRepository {
     private readonly AppDbContext _context;
 
     public ProdutoRepository(AppDbContext context): base (context) { }
+
+    public PagedList<Produto> GetProdutos(ProdutosParameters produtoParams) {
+
+        var produtos = GetAll().OrderBy(p => p.ProdutoId).AsQueryable();
+        var produtosOrdenados = PagedList<Produto>.ToPageList(produtos, produtoParams.PageNumber, produtoParams.PageSize);
+        return produtosOrdenados;
+    }
+
+    /*public IEnumerable<Produto> GetProdutos(ProdutosParameters produtoParams) {
+
+        return GetAll().OrderBy(p => p.Nome)
+            .Skip((produtoParams.PageNumber - 1)* produtoParams.PageSize)
+            .Take(produtoParams.PageSize)
+            .ToList();
+    }*/
 
     public IEnumerable<Produto> GetProdutosPorCategoria(int id) {
         
